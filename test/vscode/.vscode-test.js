c74f447a2df468c1f3e02594c078469baae289e0
Juan Cruz Viotti <jv@jviotti.com>
2025-11-06 15:19:45 -0400

    Test the VSIX on CI
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/vscode/.vscode-test.js
+++ test/vscode/.vscode-test.js
@@ -3,19 +3,23 @@ const path = require('path');
 
 const buildTestDir = path.resolve(__dirname, '../../build/test/vscode');
 
-module.exports = defineConfig({
+const config = {
   files: '../../build/test/vscode/**/*.test.js',
-  extensionDevelopmentPath: '../../vscode',
   cachePath: path.join(buildTestDir, '.vscode-test'),
   launchArgs: [
     '--user-data-dir', path.join(buildTestDir, '.vscode-test/user-data'),
-    '--extensions-dir', path.join(buildTestDir, '.vscode-test/extensions'),
-    // Add --disable-extensions to avoid interference from other extensions
-    '--disable-extensions'
+    '--extensions-dir', path.join(buildTestDir, '.vscode-test/extensions')
   ],
   mocha: {
     ui: 'tdd',
     timeout: 20000,
     color: true
   }
-});
+};
+
+if (!process.env.VSIX) {
+  config.extensionDevelopmentPath = '../../vscode';
+  config.launchArgs.push('--disable-extensions');
+}
+
+module.exports = defineConfig(config);
